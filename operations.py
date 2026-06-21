39f1bb576514519d358de3012d514047c9872b97
Yosef Israeli <yosefdan10@gmail.com>
2026-06-21 13:48:35 +0300

    הוספת פונקציות כפל וחילק

--- operations.py
+++ operations.py
@@ -8,3 +8,14 @@ def	subtract(a,	b):
     "מחסר שני מספרים"
 
     return	a	-	b
+
+def multiply(a, b):
+    "מכפיל שני מספרים"
+    return a * b
+
+def divide(a, b):
+    "מחלק שני מספרים"
+    if b == 0:
+        print("error")
+        return
+    return a / b
