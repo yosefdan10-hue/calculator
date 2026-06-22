f463d4305f2bed22e3c28a52a23cdad4257bb813
Yosef Israeli <yosefdan10@gmail.com>
2026-06-22 15:04:53 +0300

    Add constants module

---
+++ constants.py
@@ -0,0 +1,17 @@
+"""
+constants module
+================
+Mathematical constants.
+"""
+
+PI = 3.14159265359
+E = 2.71828182846
+GOLDEN_RATIO = 1.61803398875
+
+
+def show_constants():
+    """Displays all the constants"""
+    print("\n=== קבועים מתמטיים ===")
+    print(f"π (pi) = {PI}")
+    print(f"e = {E}")
+    print(f"φ (golden ratio) = {GOLDEN_RATIO}")
