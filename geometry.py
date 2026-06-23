6479cf3acfabf69a9cd3400f1293acfb69633c48
Yosef Israeli <yosefdan10@gmail.com>
2026-06-23 16:23:59 +0300

    Add geometry area functions

---
+++ geometry.py
@@ -0,0 +1,8 @@
+def circle_area(radius):
+    return 3.14 * radius * radius
+
+def rectangle_area(width, height):
+    return width * height
+
+def triangle_area(base, height):
+    return (base * height) / 2
