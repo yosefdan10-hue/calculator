a9848853e9cada918cb9ff33cb4bcc66a7e84ef1
Yosef Israeli <yosefdan10@gmail.com>
2026-06-23 16:42:18 +0300

    Add percentage functions

---
+++ percentage.py
@@ -0,0 +1,8 @@
+def percent_of(number, percent):
+    return number * percent / 100
+
+def increase_by_percent(number, percent):
+    return number * (1 + percent / 100)
+
+def decrease_by_percent(number, percent):
+    return number * (1 - percent / 100)
